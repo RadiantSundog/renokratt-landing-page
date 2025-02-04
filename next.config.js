666dddfd7bc86d80ec02495d407c7af9b5560486
Alexander Levero <Alexander.Levero@gmail.com>
2025-02-04 22:06:50 +0200

    next config fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
-  output: 'export',
+  // output: 'export', //comment out to stop static page generation, needed for email to function
   eslint: {
     ignoreDuringBuilds: true,
   },
